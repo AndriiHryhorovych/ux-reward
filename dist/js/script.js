c868537299a5c1a57b915d235723d9a3fde068f0
AndriiHryhorovych <59657519+AndriiHryhorovych@users.noreply.github.com>
2021-08-17 16:30:32 +0300

    slider added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,34 +1,5 @@
-// const swiper = new Swiper('.swiper-container', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
 
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
-// $(document).ready(function(){
-//   $('.slider-photos').slick({
-//     accessibility: true,
-//     arrows: true.
-//   });
-// });
-$('.sider-photos').slick({
-  // infinite: true,
-  // slidesToShow: 1,
-  // slidesToScroll: 1,
+$('.slider-photos').slick({
   accessibility: true,
-  arrows: true.
+  arrows: true
 });
