1cffe00b17b4951bea51fea41d8868d5a6c99c1c
AndriiHryhorovych <59657519+AndriiHryhorovych@users.noreply.github.com>
2021-08-17 15:33:53 +0300

    slick bad

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,34 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
+// const swiper = new Swiper('.swiper-container', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+// $(document).ready(function(){
+//   $('.slider-photos').slick({
+//     accessibility: true,
+//     arrows: true.
+//   });
+// });
+$('.sider-photos').slick({
+  // infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  accessibility: true,
+  arrows: true.
 });
